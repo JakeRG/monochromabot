e19c990ffd63807975b358475f869756558089cf
Jake Robin van der Ploeg <jake.r.g@gmail.com>
2021-01-17 17:09:55 +0100

    ✨ - Added deleteCommand functionality, support for toggling silent mode, made SILENT_MODE an environment variable, added support for detecting admins and moderators based on chat roles, blocked adding commands to the database with names that are hardcoded into the bot, removed the restriction of commands starting with '!', bit of cleanup and refactoring.

--- src/chatCommands.js
+++ src/chatCommands.js
@@ -1,3 +1,7 @@
+// We only import this for auto-completion reasons
+// eslint-disable-next-line no-unused-vars
+const { PrivateMessage } = require('twitch-chat-client');
+
 const CommandService = require('./commands/commandService');
 const MessageService = require('./messages/messageService');
 
@@ -5,27 +9,50 @@ const logger = require('./logger');
 
 const adminUsers = ['jake_r_g'];
 
-const SILENT_MODE = true;
+let silentMode = process.env.SILENT_MODE || true;
 
-const parseAdminCommands = async (client, channel, message) => {
-  // Change logging level
-  if (message.startsWith('!loglevel ')) {
-    const level = message.substr(message.indexOf(' ') + 1);
-    logger.debug(level);
-    if (logger.setLogLevel(level)) {
-      client.action(channel, `Log level set to ${level}.`);
-      return;
+const takenCommandNames = [
+  '!logLevel',
+  '!topChat',
+  '!silentBot',
+  '!silenceBot',
+  'shut up, bot',
+  '!speakBot',
+  'hey bot, talk',
+  '!addCommand',
+  '!updateCommand',
+  '!removeCommand',
+  '!deleteCommand',
+  '!help',
+  '!ping',
+];
+
+// Custom commands
+const runCustomCommand = async (client, channel, message, isAdmin, isMod) => {
+  try {
+    const command = await CommandService.findCommand(channel, message, isAdmin, isMod);
+    if (command) {
+      logger.debug(`Command found: ${JSON.stringify(command)}`);
+      if (command.action) {
+        client.action(channel, command.output);
+        return;
+      }
+      client.say(channel, command.output);
     }
-    client.action(channel, 'Log level could not be set.');
+  } catch (e) {
+    logger.error(`Error in runCustomCommand: ${e.message}`);
   }
+};
 
-  // DEBUG
-  // Debugging command for top chat functionality
-  if (message.startsWith('!topChat')) {
-    MessageService.topChatters('#jake_r_g');
-    return;
-  }
-
+/**
+ * Functions to manage the custom commands stored in the database.
+ *
+ * @param {*} client The chat client object used by the bot to send messages to the channel, etc
+ * @param {*} channel The channel in which the message was sent
+ * @param {*} message The message sent by the user
+ * @returns {Boolean} - True if there were any command-managing commands, false if not.
+ */
+const manageCustomCommands = async (client, channel, message) => {
   // Add commands
   if (message.startsWith('!addCommand ')) {
     try {
@@ -59,16 +86,22 @@ const parseAdminCommands = async (client, channel, message) => {
         const command = await CommandService.findCommand(channel, trigger, true, true);
         if (command) {
           client.say(channel, `Command '${trigger}' already exists!`);
-          return;
+          return true;
+        }
+
+        // Check if command is in the list of hardcoded commands
+        if (takenCommandNames.find(trigger)) {
+          client.say(channel, `Command '${trigger}' can't be altered!`);
+          return true;
         }
 
         // Add command
         await CommandService.addCommand(channel, trigger, output, adminOnly, modOnly, action);
       }
-      return;
+      return true;
     } catch (e) {
       logger.error(`Error in parseAdminCommands - addCommand: ${e.message}`);
-      return;
+      return true;
     }
   }
 
@@ -86,58 +119,130 @@ const parseAdminCommands = async (client, channel, message) => {
         const command = await CommandService.findCommand(channel, trigger, true, true);
         if (!command) {
           client.say(channel, `Command '${trigger}' does not exist!`);
-          return;
+          return true;
         }
 
         // Update command
         await CommandService.updateCommand(channel, trigger, output);
         client.say(channel, `Command ${trigger} has been updated!`);
       }
-      return;
+      return true;
     } catch (e) {
       logger.error(`Error in parseAdminCommands - updateCommand: ${e.message}`);
-      return;
+      return true;
     }
   }
 
-  // Remove commands
-  if (message.startsWith('!removeCommand ')) {
-    // TODO: implement
+  // 'Remove command' suggestion
+  if (message.startsWith('!removeCommand')) {
+    client.say(channel, 'Try !deleteCommand :)');
+  }
+
+  // Delete commands
+  if (message.startsWith('!deleteCommand ')) {
+    try {
+      let output = message.substr('!deleteCommand '.length);
+
+      const triggerEnd = output.indexOf(' ');
+      if (triggerEnd > 0) {
+        const trigger = output.substr(0, triggerEnd);
+        output = output.substr((triggerEnd + 1));
+
+        // Check if command exists
+        const command = await CommandService.findCommand(channel, trigger, true, true);
+        if (!command) {
+          client.say(channel, `Command '${trigger}' does not exist!`);
+          return true;
+        }
+
+        // Delete command
+        await CommandService.deleteCommand(channel, trigger);
+        client.say(channel, `Command ${trigger} has been deleted!`);
+      }
+      return true;
+    } catch (e) {
+      logger.error(`Error in parseAdminCommands - deleteCommand: ${e.message}`);
+      return true;
+    }
+  }
+
+  // If there were no command-managing commands, return false
+  return false;
+};
+
+const parseAdminCommands = async (client, channel, message) => {
+  // Change logging level
+  if (message.startsWith('!loglevel ')) {
+    const level = message.substr(message.indexOf(' ') + 1);
+    logger.debug(level);
+    if (logger.setLogLevel(level)) {
+      client.action(channel, `Log level set to ${level}.`);
+      return;
+    }
+    client.action(channel, 'Log level could not be set.');
+  }
+
+  // DEBUG
+  // Debugging command for top chat functionality
+  if (message.startsWith('!topChat')) {
+    MessageService.topChatters('#jake_r_g');
     return;
   }
 
-  // Check other commands
-  try {
-    // TODO: make isAdmin and isMod check user's status, then move this to parseCommands below
-    const isAdmin = true;
-    const isMod = true;
-    const command = await CommandService.findCommand(channel, message, isAdmin, isMod);
-    if (command) {
-      console.log(`command found: ${JSON.stringify(command)}`); // TODO: remove
-      if (command.action) {
-        client.action(channel, command.output);
-        return;
-      }
-      client.say(channel, command.output);
-    }
-  } catch (e) {
-    logger.error(`Error in parseAdminCommands - other commands: ${e.message}`);
+  // Put bot in silent mode
+  if (message.startsWith('!silentBot') || message.startsWith('!silenceBot') || message.startsWith('shut up, bot')) {
+    silentMode = true;
+    client.action(channel, 'zips mouth');
+  }
+
+  // Take bot out of silent mode
+  if (message.startsWith('!speakBot') || message.startsWith('hey bot, talk')) {
+    silentMode = false;
+    client.say(channel, 'Hello!');
+  }
+
+  if (manageCustomCommands) {
+    return;
+  }
+
+  // Only check for custom commands here while in silent mode.
+  // This allows admins to keep control even when in silent mode.
+  if (silentMode) {
+    runCustomCommand(client, channel, message, true, true);
   }
 };
 
-const parseCommands = (client, channel, message) => {
-  if (!SILENT_MODE) {
-    if (message.toLowerCase() === ('!help')) {
-      client.action(channel, 'Yo, I\'m just a silly WIP, what do you want from me');
-      return;
-    }
-    if (message.toLowerCase() === ('!ping')) {
-      client.say(channel, 'Pong!');
-      // return; // TODO: enable once other commands parsing is moved from parseAdminCommands to here
-    }
+/**
+ * Function to parse commands - only runs while NOT in silent mode!
+ * @param {*} client The chat client object used by the bot to send messages to the channel, etc
+ * @param {*} channel The channel in which the message was sent
+ * @param {*} message The message sent by the user
+ * @param {*} isAdmin True if the user is a bot admin, or broadcaster in the channel
+ * @param {*} isMod True if the user is a bot admin, or broadcaster or mod in the channel
+ */
+const parseCommands = (client, channel, message, isAdmin, isMod) => {
+  if (message.toLowerCase() === ('!help')) {
+    client.action(channel, 'Yo, I\'m just a silly WIP, what do you want from me');
+    return;
   }
+  if (message.toLowerCase() === ('!ping')) {
+    client.say(channel, 'Pong!');
+    return;
+  }
+
+  // Check custom commands
+  runCustomCommand(client, channel, message, isAdmin, isMod);
 };
 
+/**
+ * Function that detects, parses and handles any commands that might be sent in chat.
+ *
+ * @param {*} client The chat client object used by the bot to send messages to the channel, etc
+ * @param {*} channel The channel in which the message was sent
+ * @param {*} user The user that sent the message
+ * @param {*} message The message sent by the user
+ * @param {PrivateMessage} privMsg PrivateMessage object containing more information
+ */
 const handleCommands = (client, channel, user, message, privMsg) => {
   const self = (user.toLowerCase() === process.env.USERNAME.toLowerCase());
 
@@ -148,10 +253,27 @@ const handleCommands = (client, channel, user, message, privMsg) => {
   logger.debug(`privMsg: ${JSON.stringify(privMsg)}`);
   logger.debug(`Self: ${JSON.stringify(self)}`);
 
-  if (!self && (message.substr(0, 1) === '!')) { // TODO: remove ! requirement?
-    parseCommands(client, channel.substr(1), message);
-    if (adminUsers.includes(user)) {
+  const chatUser = privMsg.userInfo;
+  // TODO: implement rate limitation here?
+
+  if (!self) {
+    let isAdmin = false;
+    let isMod = false;
+    // Check if the user is either a bot admin or a broadcaster in the channel
+    if (adminUsers.includes(user) || chatUser.isBroadcaster) {
+      isAdmin = true;
+      isMod = true;
+      // Run any hardcoded admin commands, and override silent mode
       parseAdminCommands(client, channel.substr(1), message);
+    }
+    // Check if the user is a moderator in the channel
+    if (chatUser.isMod) {
+      isMod = true;
+    }
+
+    // Check any commands that are not hardcoded.
+    if (!silentMode) {
+      parseCommands(client, channel.substr(1), message, isAdmin, isMod);
     }
   }
 };
